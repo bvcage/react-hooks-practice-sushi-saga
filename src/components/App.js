56e73cb047c530644f5ccfca1a793e1d349b0a20
Bailey Cage <bailey.cage@gmail.com>
2022-09-08 16:45:17 -0400

    enable sushi wallet form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
+import SushiWallet from "./SushiWallet";
 
 const API = "http://localhost:3001/sushis";
 
@@ -24,10 +25,16 @@ function App() {
     } else return false;
   }
 
+  function onAddMoney (addAmt) {
+    if (typeof addAmt !== 'number') addAmt = parseInt(addAmt);
+    setBudget(budget + addAmt);
+  }
+
   return (
     <div className="app">
       <SushiContainer sushiAry={sushiAry} onEat={handleEatSushi} />
       <Table plates={emptiesAry} budget={budget} />
+      <SushiWallet onAddMoney={onAddMoney} />
     </div>
   );
 }
